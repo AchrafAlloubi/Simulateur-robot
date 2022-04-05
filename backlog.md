b6db418420dd7de40444fb744a8312b7ea51ec44
william <mail@williamperron.com>
2022-04-04 23:14:46 -0400

    Enlever backlog Enlever todo

--- backlog.md
+++
@@ -1,5 +0,0 @@
-# TODO
-- Système expert (Filtrage, Choix d'une règle, Appliquer la règle)
-- Déterministe (random de rester pris dans les décombres)
-- Devrait mieux choisir la prochaine case si détecte pas de poussière/chaleur/son
-- Devrait jamais revenir sur son chemin
--- expert_system/facts.py
+++ expert_system/facts.py
@@ -1,16 +1,6 @@
 """
 Connaissances assertionnelles (Les faits)
-
-todo : Liste des faits:
-Position du robot
-Nombre de chaleur à proximité
-Nombre de poussière à proximité
-Est piégé
-A sauvé le survivant
 """
-# todo se_situer(singe, D)
-
-from simulator.environment import Environment
 
 
 class Facts:
